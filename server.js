2e9e6dd33681622e557fe7ed745087b7c4ece601
Noesax <marko.rique@unmsm.edu.pe>
2023-10-26 18:19:11 -0500

    comentario

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { config } = require('dotenv');
 const app = express();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
 });
 
 app.use(cors());
@@ -14,67 +14,67 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/registro', (req, res) => {
-  pool.query('SELECT * FROM cliente')
-    .then(response => {
-      console.log(response.rows);
-      res.json(response.rows);
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: 'Error en la consulta a la base de datos' });
-    });
+    pool.query('SELECT * FROM cliente')
+        .then(response => {
+            console.log(response.rows);
+            res.json(response.rows);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Error en la consulta a la base de datos' });
+        });
 });
 
 app.post('/registro', (req, res) => {
-  const { dni, nombre, apellido, fechaNacimiento, distrito, departamento, fechaAfiliacion, email } = req.body;
-  const query = 'INSERT INTO cliente (dni, nombre, apellido, fechanac, distrito, departamento, fechaafili, correo) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
-  const values = [dni, nombre, apellido, fechaNacimiento, distrito, departamento, fechaAfiliacion, email];
+    const { dni, nombre, apellido, fechaNacimiento, distrito, departamento, fechaAfiliacion, email } = req.body;
+    const query = 'INSERT INTO cliente (dni, nombre, apellido, fechanac, distrito, departamento, fechaafili, correo) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
+    const values = [dni, nombre, apellido, fechaNacimiento, distrito, departamento, fechaAfiliacion, email];
 
-  pool.query(query, values)
-    .then(() => {
-      res.status(201).json({ message: 'Registro exitoso ' });
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: 'Error en el registro' });
-    });
+    pool.query(query, values)
+        .then(() => {
+            res.status(201).json({ message: 'Registro exitoso ' });
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Error en el registro' });
+        });
 });
 
 app.post('/login', (req, res) => {
-  const { dni, correo } = req.body;
-  const query = 'SELECT * FROM cliente WHERE dni = $1 AND correo = $2';
-  const values = [dni, correo];
+    const { dni, correo } = req.body;
+    const query = 'SELECT * FROM cliente WHERE dni = $1 AND correo = $2';
+    const values = [dni, correo];
 
-  pool.query(query, values)
-    .then(response => {
-      if (response.rows.length === 1) {
-        res.status(200).json({ message: 'Inicio de sesión exitoso' });
-      } else {
-        res.status(401).json({ error: 'Credenciales incorrectas' });
-      }
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: 'Error en el inicio de sesión' });
-    });
+    pool.query(query, values)
+        .then(response => {
+            if (response.rows.length === 1) {
+                res.status(200).json({ message: 'Inicio de sesión exitoso' });
+            } else {
+                res.status(401).json({ error: 'Credenciales incorrectas' });
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Error en el inicio de sesión' });
+        });
 });
 
 app.get('/productos', (req, res) => {
-  pool.query('SELECT * FROM producto')
-    .then(response => {
-      console.log(response.rows);
-      res.json(response.rows);
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: 'Error en la consulta de productos' });
-    });
+    pool.query('SELECT * FROM producto')
+        .then(response => {
+            console.log(response.rows);
+            res.json(response.rows);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Error en la consulta de productos' });
+        });
 });
 
 app.get('/login', (req, res) => {
-  res.send('Página de inicio de sesión');
+    res.send('Página de inicio de sesión');
 });
 
 app.listen(8081, () => {
-  console.log('Listening en el puerto 8081');
+    console.log('Listening en el puerto 8081');
 });
